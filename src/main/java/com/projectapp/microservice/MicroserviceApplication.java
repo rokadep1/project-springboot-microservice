b1b780fe386196e22203ee0d4c4819734f27b258
rokadep1 <125141803+rokadep1@users.noreply.github.com>
2025-07-13 17:47:18 +0530

    Update MicroserviceApplication.java

--- src/main/java/com/projectapp/microservice/MicroserviceApplication.java
+++ src/main/java/com/projectapp/microservice/MicroserviceApplication.java
@@ -2,20 +2,20 @@ package com.projectapp.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MicroserviceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceApplication.class, args);
+    }
 }
+
 @RestController
-public class GreetingController {
+class GreetingController {
     @GetMapping("/greeting")
     public String greet() {
         return "Hello from ABC Technologies!";
     }
 }
-
